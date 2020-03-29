880c295a42ec957cafb61bf14d7616ae710d92a2
YoshioKondo <yoshio15310@gmail.com>
2020-03-29 17:43:36 +0900

    作品投稿用API新規作成

---
+++ functions/works/post-work/app.js
@@ -0,0 +1,68 @@
+'use strict';
+
+const aws = require('aws-sdk');
+const docClient = new aws.DynamoDB.DocumentClient({ region: 'ap-northeast-1' });
+
+const postWork = event => {
+  return new Promise((resolve, reject) => {
+    const params = {
+      TableName: 'works-table',
+      Item: formatItem(JSON.parse(event.body))
+    };
+    docClient.put(params, function (err, data) {
+      if (err) {
+        console.log(err);
+        reject(err);
+      } else {
+        console.log(data.Items);
+        resolve(data.Items)
+      }
+    })
+  })
+}
+
+const generateRespons = (items, status) => {
+  const response = {
+    "statusCode": status,
+    "headers": {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true"
+    },
+    "body": items,
+    "isBase64Encoded": false
+  };
+  return response
+}
+
+const formatItem = req => {
+  return {
+    'work_id': getUniqueId(),
+    'title': req.title,
+    'description': req.description,
+    'user_id': '0001', // req.userId,
+    'user_name': 'testuser', // req.userName,
+    'posted_at': getUnixTime()
+  }
+}
+
+const getUniqueId = () => {
+  return Math.floor(1000 * Math.random()).toString(16) + Date.now().toString(16)
+}
+
+const getUnixTime = () => {
+  const now = new Date()
+  return Math.floor(now.getTime() / 1000)
+}
+
+exports.lambdaHandler = function (event, context, callback) {
+  postWork(event)
+    .then(res => {
+      const response = generateRespons(JSON.stringify(res), 200)
+      callback(null, response);
+    })
+    .catch(err => {
+      const response = generateRespons(JSON.stringify(err), 500)
+      callback(null, response);
+    })
+}; 
