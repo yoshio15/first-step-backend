3a9dc83116f925d4ebcc054ce5244a23c2f2fc7b
YoshioKondo <yoshio15310@gmail.com>
2020-05-24 15:23:44 +0900

    ユーザアイコンアップロード用API追加

---
+++ functions/users/get-s3-presigned-url-for-user-icon/app.js
@@ -0,0 +1,50 @@
+const AWS = require('aws-sdk');
+const S3 = new AWS.S3();
+const BUCKET_NAME = 'dev-first-step-users';
+
+AWS.config.region = 'ap-northeast-1';
+
+const getS3PresignedUrl = reqBody => {
+  console.info('REQUEST_BODY: ' + JSON.stringify(reqBody))
+  return new Promise((resolve, reject) => {
+    const params = {
+      Bucket: BUCKET_NAME,
+      Key: reqBody.user_id,
+      ContentType: `${reqBody.content}/${reqBody.type}`,
+      Expires: 10
+    };
+    S3.getSignedUrl('putObject', params, function (err, url) {
+      if (err) return reject(err)
+      resolve(url);
+    });
+  })
+}
+
+const generateResponse = (items, status) => {
+  const response = {
+    "statusCode": status,
+    "headers": {
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": "true"
+    },
+    "body": items,
+    "isBase64Encoded": false
+  };
+  return response
+}
+
+exports.lambdaHandler = function (event, context, callback) {
+  console.info(`event: ${JSON.stringify(event)}`)
+  getS3PresignedUrl(event.pathParameters)
+    .then(res => {
+      const response = generateResponse(JSON.stringify(res), 200)
+      console.info(`response: ${res}`)
+      callback(null, response);
+    })
+    .catch(err => {
+      const response = generateResponse(JSON.stringify(err), 500)
+      console.error(`error: ${err}`)
+      callback(null, response);
+    })
+}; 
