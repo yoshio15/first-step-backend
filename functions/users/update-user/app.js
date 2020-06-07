35ca680debc03c7e06f5a4e43a317427242dac39
YoshioKondo <yoshio15310@gmail.com>
2020-06-07 20:34:22 +0900

    ユーザ更新時のプロパティをリクエストに従って動的に変更する様修正

--- functions/users/update-user/app.js
+++ functions/users/update-user/app.js
@@ -4,20 +4,9 @@ const aws = require('aws-sdk');
 const docClient = new aws.DynamoDB.DocumentClient({ region: 'ap-northeast-1' });
 
 const updateUser = (reqBody) => {
+  console.info(`REQUEST_BODY: ${JSON.stringify(reqBody)}`)
   return new Promise((resolve, reject) => {
-    const params = {
-      TableName: 'users-table',
-      Key: { 'user_id': reqBody.userId },
-      ExpressionAttributeNames: {
-        '#un': 'user_name',
-        '#us': 'user_summary'
-      },
-      ExpressionAttributeValues: {
-        ':userName': reqBody.userName,
-        ':userSummary': reqBody.userSummary
-      },
-      UpdateExpression: 'SET #un=:userName, #us=:userSummary'
-    };
+    const params = generateReqParams(reqBody)
     docClient.update(params, function (err, data) {
       if (err) {
         console.log(err);
@@ -28,6 +17,40 @@ const updateUser = (reqBody) => {
       }
     })
   })
+}
+
+const generateReqParams = reqBody => {
+  let params = {
+    TableName: 'users-table',
+    Key: { 'user_id': reqBody.userId },
+    ExpressionAttributeNames: {},
+    ExpressionAttributeValues: {},
+    UpdateExpression: ''
+  }
+  let updateExpressionArray = []
+  const userName = reqBody.userName
+  const userSummary = reqBody.userSummary
+  const userIconImg = reqBody.userIconImg
+  if (userName) {
+    params.ExpressionAttributeNames['#un'] = 'user_name'
+    params.ExpressionAttributeValues[':userName'] = userName
+    updateExpressionArray.push('#un=:userName')
+  }
+  if (userSummary) {
+    params.ExpressionAttributeNames['#us'] = 'user_summary'
+    params.ExpressionAttributeValues[':userSummary'] = userSummary
+    updateExpressionArray.push('#us=:userSummary')
+  }
+  if (userIconImg) {
+    params.ExpressionAttributeNames['#uii'] = 'user_icon_img'
+    params.ExpressionAttributeValues[':userIconImg'] = userIconImg
+    updateExpressionArray.push('#uii=:userIconImg')
+  }
+  if (updateExpressionArray.length > 0) {
+    params.UpdateExpression = 'SET ' + updateExpressionArray.join(', ')
+  }
+  console.info(`UPDATE_PARAMS: ${JSON.stringify(params)}`)
+  return params
 }
 
 const generateResponse = (items, status) => {
